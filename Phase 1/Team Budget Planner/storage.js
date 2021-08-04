1794c5acfa5f7eae958c315d9bd4092c37bed7bf
Anthony Ho <AnthonyHo0728@gmail.com>
2021-08-04 07:37:02 -0700

    main functionality complete

--- Phase 1/Team Budget Planner/storage.js
+++ Phase 1/Team Budget Planner/storage.js
@@ -6,6 +6,10 @@ function storeData(cName, pName, budget) {
         sessionStorage.setItem(0,JSON.stringify(jsonArr));
     } else {
         let newJSON = sessionStorage.getItem(0);
+        // let test = JSON.parse(newJSON);
+        // console.log(test);
+        // jsonArr = []
+        // jsonArr.push(test);
         jsonArr.push(jsonObj);
         sessionStorage.setItem(0,JSON.stringify(jsonArr));
     }
@@ -13,17 +17,17 @@ function storeData(cName, pName, budget) {
 
 function getData() {
     let jsonData = sessionStorage.getItem(0);
+    console.log(jsonData)
     let test = JSON.parse(jsonData);
-    // var table = document.getElementById("myTable");
+    var table = document.getElementById("myTable");
     for(var i = 0 ; i < test.length ; i++) {
-        // var row = table.insertRow(0);
-        console.log(test[i].client);
-        console.log(test[i].project)
-        console.log(test[i].budgetVal)
-        // //console.log(JSON.stringify(test[i]));
-        //  console.log(currRow.client.value)
-        //  console.log(blah.project)
-        //  console.log(blah.budgetVal)
+        var row = table.insertRow(1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        cell1.innerHTML = test[i].client;
+        cell2.innerHTML = test[i].project;
+        cell3.innerHTML = test[i].budgetVal;
     }
 }
 // Pass a json array into session storage instead of each individual input. In the store data function, pull the array, then edit the array and put the array back into
