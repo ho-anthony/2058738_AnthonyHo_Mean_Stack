10f31775a3d15e5f3a92def3a177fef30b16b81f
Anthony Ho <AnthonyHo0728@gmail.com>
2021-08-11 14:39:11 -0700

    Done

--- Phase 2/Cart Management/shoppingCart.js
+++ Phase 2/Cart Management/shoppingCart.js
@@ -1,6 +1,9 @@
 var jsonArr = new Array;
 function addCart(name, val) {
-    var currCount = parseInt(document.getElementById("cartValue").innerHTML);
+    // let currCount:number = parseInt(document.getElementById("cartValue").innerHTML);
+    // currCount++;
+    // document.getElementById("cartValue").innerHTML = ""+currCount;
+    var currCount = parseInt(sessionStorage.getItem("1"));
     currCount++;
     document.getElementById("cartValue").innerHTML = "" + currCount;
     var jsonObj = { item: name, price: val };
@@ -18,6 +21,29 @@ function addCart(name, val) {
         jsonArr.push(jsonObj);
         sessionStorage.setItem("0", JSON.stringify(jsonArr));
     }
-    var test = sessionStorage.getItem("0");
-    console.log(JSON.parse(test));
+    sessionStorage.setItem("1", "" + currCount);
+}
+function setInitialCount() {
+    if (sessionStorage.getItem("1") === null) {
+        sessionStorage.setItem("1", "" + 0);
+    }
+    else {
+        var currCount = parseInt(sessionStorage.getItem("1"));
+        document.getElementById("cartValue").innerHTML = "" + currCount;
+    }
+}
+function getList() {
+    var jsonData = sessionStorage.getItem("0");
+    var parsedJSON = JSON.parse(jsonData);
+    var table = document.getElementById("myTable");
+    var total = 0;
+    for (var i = 0; i < parsedJSON.length; i++) {
+        var row = table.insertRow(1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        cell1.innerHTML = parsedJSON[i].item;
+        cell2.innerHTML = parsedJSON[i].price;
+        total += parseInt(parsedJSON[i].price, 10);
+    }
+    document.getElementById("totalVal").innerHTML = "" + total;
 }
