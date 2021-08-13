1d9828272f371f84187ced5951e5cb22d1964d40
Anthony Ho <AnthonyHo0728@gmail.com>
2021-08-13 13:53:47 -0700

    basic login and registration

--- Phase 2/Portfolio/src/app/app.component.ts
+++ Phase 2/Portfolio/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -6,6 +6,29 @@ import { NgForm } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Portfolio';
+export class AppComponent implements OnInit{
+  constructor() {}
+  ngOnInit(): void {}
+  loginMsg:String = "";
+  registerMsg:String="";
+  
+  checkLogin(loginRef:NgForm) {
+    let login = loginRef.value;
+    console.log(login.username + " " + login.password);
+    if(login.username == "username" && login.password == "password") {
+      this.loginMsg = "login success";
+    } else {
+      this.loginMsg = "login failed"
+    }
+  }
+
+  checkRegister(registerRef:NgForm) {
+    let registerVal = registerRef.valid;
+    if(registerVal) {
+      this.registerMsg = "Successful Registration"
+    } else {
+      this.registerMsg = "Please fill in all fields"
+    }
+  }
+
 }
