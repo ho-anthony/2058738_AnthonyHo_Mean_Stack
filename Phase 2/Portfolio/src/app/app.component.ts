9f13d2e724ecde1f46cf5fe16cb6583b8eff0950
Anthony Ho <AnthonyHo0728@gmail.com>
2021-08-16 13:36:58 -0700

    hide/show implemenetation

--- Phase 2/Portfolio/src/app/app.component.ts
+++ Phase 2/Portfolio/src/app/app.component.ts
@@ -11,18 +11,33 @@ export class AppComponent implements OnInit{
   ngOnInit(): void {}
   loginMsg:String = "";
   registerMsg:String="";
+  showLogin = true;
+  showRegister = false;
+  showPortfolio = false;
   
-  checkLogin(loginRef:NgForm) {
+  checkLogin(loginRef:NgForm):void {
     let login = loginRef.value;
     console.log(login.username + " " + login.password);
     if(login.username == "username" && login.password == "password") {
       this.loginMsg = "login success";
+      this.showLogin = false;
+      this.showPortfolio = true;
     } else {
       this.loginMsg = "login failed"
     }
   }
 
-  checkRegister(registerRef:NgForm) {
+  navRegister():void {
+    this.showLogin = false;
+    this.showRegister = true;
+  }
+
+  navLogin():void {
+    this.showLogin = true;
+    this.showRegister = false;
+  }
+
+  checkRegister(registerRef:NgForm):void {
     let registerVal = registerRef.valid;
     if(registerVal) {
       this.registerMsg = "Successful Registration"
