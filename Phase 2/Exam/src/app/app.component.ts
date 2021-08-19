81454d005fa9d7ad3bb5f1ef63c0d36588d3bb14
Anthony Ho <AnthonyHo0728@gmail.com>
2021-08-18 23:09:13 -0700

    Connected json file

--- Phase 2/Exam/src/app/app.component.ts
+++ Phase 2/Exam/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-// import * as qJson from '../assets/questions.json';
+import qJson from '../assets/questions.json';
 
 @Component({
   selector: 'app-root',
@@ -12,18 +12,14 @@ export class AppComponent implements OnInit{
   numCorrect:String = "";
   passed:boolean = true;
   showResult:boolean = false;
-  tempJson = 
-  [{"QNum":"1", "Question":"1+2?", "Opt1":"12", "Opt2":"1", "Opt3":"2", "Opt4":"3", "Ans":"3", "corr":""},
-  {"QNum":"2", "Question":"3+4?", "Opt1":"1", "Opt2":"7", "Opt3":"42", "Opt4":"2", "Ans":"7", "corr":""},
-  {"QNum":"3", "Question":"5+6?", "Opt1":"68", "Opt2":"45", "Opt3":"11", "Opt4":"3", "Ans":"11", "corr":""}]
-  test1 = ""
-  test2 = ""
+  qList? = qJson;
+  
   constructor(public form:FormBuilder) {
     this.questionList=form.group({});
   }
 
   ngOnInit():void {
-    this.tempJson.forEach(q=> {
+    this.qList.forEach((q: { Question: string; })=> {
       this.questionList.addControl(q.Question,this.form.control(""));
     })
   }
@@ -31,8 +27,7 @@ export class AppComponent implements OnInit{
   checkAnswer():void {
     let answerList = this.questionList.value;
     let counter:number = 0;
-    //console.log(answerList["1+2?"]);
-    this.tempJson.forEach(element => {
+    this.qList.forEach((element: { Question: any; Ans: string; corr: string; }) => {
       let currQ = element.Question;
       if(answerList[currQ] == element.Ans) {
         element.corr = "Correct!"
@@ -43,7 +38,7 @@ export class AppComponent implements OnInit{
     });
     this.numCorrect = ""+counter;
     this.showResult = true;
-    if(counter >= 3) {
+    if(counter >= 7) {
       this.passed = true;
     } else {
       this.passed = false;
