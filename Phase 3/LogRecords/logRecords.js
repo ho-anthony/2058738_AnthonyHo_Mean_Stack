d9f42d277b5f81c79cc3360ca6f3367b8b26faf6
Anthony Ho <AnthonyHo0728@gmail.com>
2021-08-24 14:06:20 -0700

    Added debug

--- Phase 3/LogRecords/logRecords.js
+++ Phase 3/LogRecords/logRecords.js
@@ -6,10 +6,12 @@ try{
     let fullList = JSON.parse(dataLog);
     fullList.push(newJson());
     let formatResult = JSON.stringify(fullList);
+    debugger;
     fs.writeFileSync("log.json",formatResult);
 } catch (err) {
     fs.writeFileSync("log.json","");
     let list = [newJson()];
+    debugger;
     let formatResult = JSON.stringify(list);
     fs.writeFileSync("log.json",formatResult);
 }
@@ -21,6 +23,7 @@ function newJson() {
     let mail = rl.questionEMail("Enter your email: ");
     let currDate = new Date();
     let dateEntry = (currDate.getMonth()+1) + "/" + currDate.getDate() + "/" + currDate.getFullYear();
+    debugger;
     let timeEntry = currDate.getHours() +":"+currDate.getMinutes()+":"+currDate.getSeconds();
     let jsonForm = {firstName:fname,lastName:lname,gender:gen,email:mail,date:dateEntry,time:timeEntry}
     return jsonForm;
