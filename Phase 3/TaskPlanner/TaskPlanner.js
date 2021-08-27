0975456004db052925f3d8af6ccad5e12a205495
Anthony Ho <AnthonyHo0728@gmail.com>
2021-08-27 14:27:45 -0700

    filter

--- Phase 3/TaskPlanner/TaskPlanner.js
+++ Phase 3/TaskPlanner/TaskPlanner.js
@@ -1,5 +1,5 @@
 let http = require("http");
- let url = require("url");
+let url = require("url");
 let fs = require("fs");
 let plannerPage = `
 <!DOCTYPE html>
@@ -12,7 +12,7 @@ let plannerPage = `
 </head>
 <body>
     <h2 style="text-align:center">Task Planner</h2>
-    <form action="add">
+    <form action="addTask">
         <h2>Add Task</h2>
         <label>Emp Id: </label>
         <input type="text" name="eId"/><br/>
@@ -23,14 +23,13 @@ let plannerPage = `
         <label>Deadline: </label>
         <input type="text" name="deadline"/><br/>
         <input type="submit" value="Add Task"/>
-       <input type="reset" value="reset"/> <br/>
     </form>
     <br/><br/>
-    <form action="del">
+    <form action="delTask">
         <h2>Delete Task</h2>
         <label>Task Id: </label>
         <input type="text" name="tId"/>
-        <input type="submit" value="Add Task"/>
+        <input type="submit" value="Delete Task"/>
     </form>
     <br/><br/>
     <h2>List Tasks</h2>
@@ -40,24 +39,41 @@ let plannerPage = `
 </html> 
 `
 let server = http.createServer((request,response) => {
-    let urlInfo = url.parse(request.url,true);
-    if(urlInfo.pathname == "/add") {
+     let urlInfo = url.parse(request.url,true);
+    if(urlInfo.pathname == "/addTask") {
         let data = urlInfo.query;
         let newJson = {"empId":data.eId, "taskId":data.tId, "taskName":data.tName, "deadline":data.deadline}
-        console.log(newJson);
         try{
             currData = fs.readFileSync("taskList.json");
             let writeList = JSON.parse(currData);
             writeList.push(newJson);
             fs.writeFileSync("taskList.json",JSON.stringify(writeList));
+            response.write(plannerPage);
         } catch(err) {
-            console.log(err);
             let writeList = [newJson];
             fs.writeFileSync("taskList.json",JSON.stringify(writeList));
+            response.write(plannerPage);
+        }
+    } else if(urlInfo.pathname == "/delTask") {
+        let data = urlInfo.query;
+        try {
+            currData = fs.readFileSync("taskList.json");
+            let list = JSON.parse(currData);
+            let newList = list.filter((iter) => {return iter.taskId !== data.tId});
+            // *************
+            // Incomplete. To-do: 
+            // Find if taskID exists in json if not, print entry does not exist
+            // Replace with new list
+            // *************
+            
+        } catch(err) {
+            // file doesnt exist, print "entry does not exist"
         }
     } else {
-        response.write(plannerPage);
+            // needs one more if statement for printing table on button press
+         response.write(plannerPage);
     }
+    //response.end();
 })
 
 server.listen(9090, () => console.log("Server running on port number 9090"))
