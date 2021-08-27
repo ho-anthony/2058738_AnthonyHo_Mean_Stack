fe34db23c978632c313e6715a02de73b6036aed5
Anthony Ho <AnthonyHo0728@gmail.com>
2021-08-27 12:53:34 -0700

    Add Task Complete

--- Phase 3/TaskPlanner/TaskPlanner.js
+++ Phase 3/TaskPlanner/TaskPlanner.js
@@ -1,5 +1,6 @@
 let http = require("http");
-// let url = require("url");
+ let url = require("url");
+let fs = require("fs");
 let plannerPage = `
 <!DOCTYPE html>
 <html lang="en">
@@ -11,7 +12,7 @@ let plannerPage = `
 </head>
 <body>
     <h2 style="text-align:center">Task Planner</h2>
-    <form>
+    <form action="add">
         <h2>Add Task</h2>
         <label>Emp Id: </label>
         <input type="text" name="eId"/><br/>
@@ -25,7 +26,7 @@ let plannerPage = `
        <input type="reset" value="reset"/> <br/>
     </form>
     <br/><br/>
-    <form>
+    <form action="del">
         <h2>Delete Task</h2>
         <label>Task Id: </label>
         <input type="text" name="tId"/>
@@ -39,7 +40,24 @@ let plannerPage = `
 </html> 
 `
 let server = http.createServer((request,response) => {
-    response.write(plannerPage);
+    let urlInfo = url.parse(request.url,true);
+    if(urlInfo.pathname == "/add") {
+        let data = urlInfo.query;
+        let newJson = {"empId":data.eId, "taskId":data.tId, "taskName":data.tName, "deadline":data.deadline}
+        console.log(newJson);
+        try{
+            currData = fs.readFileSync("taskList.json");
+            let writeList = JSON.parse(currData);
+            writeList.push(newJson);
+            fs.writeFileSync("taskList.json",JSON.stringify(writeList));
+        } catch(err) {
+            console.log(err);
+            let writeList = [newJson];
+            fs.writeFileSync("taskList.json",JSON.stringify(writeList));
+        }
+    } else {
+        response.write(plannerPage);
+    }
 })
 
 server.listen(9090, () => console.log("Server running on port number 9090"))
