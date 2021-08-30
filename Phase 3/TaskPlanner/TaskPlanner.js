999a201f543bdb438698238d61f17f05057b32bd
Anthony Ho <AnthonyHo0728@gmail.com>
2021-08-30 13:57:31 -0700

    Completed

--- Phase 3/TaskPlanner/TaskPlanner.js
+++ Phase 3/TaskPlanner/TaskPlanner.js
@@ -40,54 +40,6 @@ let plannerPage = `
 </body>
 </html> 
 `
-let tablePage = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h2 style="text-align:center">Task Planner</h2>
-    <form action="addTask">
-        <h2>Add Task</h2>
-        <label>Emp Id: </label>
-        <input type="text" name="eId"/><br/>
-        <label>Task Id: </label>
-        <input type="text" name="tId"/><br/>
-        <label>Task: </label>
-        <input type="text" name="tName"/><br/>
-        <label>Deadline: </label>
-        <input type="text" name="deadline"/><br/>
-        <input type="submit" value="Add Task"/>
-    </form>
-    <br/><br/>
-    <form action="delTask">
-        <h2>Delete Task</h2>
-        <label>Task Id: </label>
-        <input type="text" name="tId"/>
-        <input type="submit" value="Delete Task"/>
-    </form>
-    <br/><br/>
-    <h2>List Tasks</h2>
-    <form action="showTable">
-        <input type="submit" value="List All Tasks"/>
-    </form>
-    <table border = 1 id="taskTable">
-    <thead>
-        <tr>
-            <th>Emp Id</th>
-            <th>Task Id</th>
-            <th>Task</th>
-            <th>Deadline</th>
-        </tr>
-    </thead>
-    </table>
-</body>
-</html> 
-`
 
 let server = http.createServer((request,response) => {
      let urlInfo = url.parse(request.url,true);
@@ -126,7 +78,7 @@ let server = http.createServer((request,response) => {
             let strNewList = JSON.stringify(newList);
             if(strList == strNewList) {
                 response.writeHead(404,{"content-type":"text/html"});
-                response.write("Delete failed. Entry does not exist.");
+                response.write("Delete failed. Task does not exist.");
                 response.write(plannerPage);
             } else {
                 fs.writeFileSync("taskList.json",strNewList);
@@ -140,6 +92,41 @@ let server = http.createServer((request,response) => {
             response.write(plannerPage);
         }
     } else if(urlInfo.pathname == "/showTable"){
+        let jsonData=[];
+        try{
+            currData = fs.readFileSync("taskList.json");
+            jsonData = JSON.parse(currData);
+        } catch {
+            fs.writeFileSync("taskList.json","[]");
+        }
+        let tablePage = `
+            <table cellpadding = 10 border = 1 style="text-align:center">
+            <thead>
+                <tr>
+                    <th>Emp Id</th>
+                    <th>Task Id</th>
+                    <th>Task</th>
+                    <th>Deadline</th>
+                </tr>
+            </thead>
+            <tbody>`
+
+        for(let i = 0 ; i < jsonData.length ; i++) {
+            let newRow = `
+                <tr>
+                    <td>`+jsonData[i].empId+`</td>
+                    <td>`+jsonData[i].taskId+`</td>
+                    <td>`+jsonData[i].taskName+`</td>
+                    <td>`+jsonData[i].deadline+`</td>
+                </tr>
+            `
+            tablePage += newRow;
+        }
+        tablePage += `
+            </tbody>
+            </table>`
+        response.writeHead(200,{"content-type":"html"});
+        response.write(plannerPage);
         response.write(tablePage);
     } else {
          response.write(plannerPage);
@@ -147,4 +134,6 @@ let server = http.createServer((request,response) => {
     response.end();
 })
 
-server.listen(9090, () => console.log("Server running on port number 9090"))
+server.listen(9090, () => console.log("Server running on port number 9090"));
+
+
