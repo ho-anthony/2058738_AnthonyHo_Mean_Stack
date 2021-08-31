fd2bba7fecefef28457fae954c36f0c159b3b2f6
Anthony Ho <AnthonyHo0728@gmail.com>
2021-08-31 14:07:17 -0700

    Functionality complete

--- Phase 3/Chat/chatClient.js
+++ Phase 3/Chat/chatClient.js
@@ -6,7 +6,7 @@ let ws = new WebSocket("ws://localhost:9090");
 
 function getMessage(uName,uMessage) {
     let newMessage = uName.value + " says: " + uMessage.value;
-    ws.send(newMessage);
+    ws.send(uMessage.value);
     let chatLog = document.getElementById("chat");
     let newLine = document.createElement("p");
     newLine.innerHTML = newMessage;
--- Phase 3/Chat/server.js
+++ Phase 3/Chat/server.js
@@ -2,7 +2,8 @@ let express = require("express");
 let app = express();
 
 let ws = require("express-ws")(app);
-
+let common = ["Hi","Hello","How Are you?","Tell me a fact", "Are you human?", "What's your favorite color?", "1+1", "What's your name?"];
+let replies = ["Hey there!", "What's up", "I'm good, how about you?","The sky is blue", "No, I am a bot", "Purple", "2", "Bot Bottington"];
 app.ws("/",(socket,request)=> {
     console.log("Client Connected!");
 
@@ -12,11 +13,14 @@ app.ws("/",(socket,request)=> {
         let dateEntry = (currDate.getMonth()+1) + "/" + currDate.getDate() + "/" + currDate.getFullYear();
         let timeEntry = currDate.getHours() +":"+currDate.getMinutes()+":"+currDate.getSeconds();
         let reply = "Message received on: " + dateEntry + " at " + timeEntry;
-        console.log(msg);
+        console.log("Client says: " + msg);
+        let replyIndex = common.findIndex(m => m==msg);
+        if(replyIndex != -1) {
+            reply = replies[replyIndex]
+        }
         console.log("Server Replied: " + reply);
         socket.send(reply);
     });
-
     
 })
 
