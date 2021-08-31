780dfa286b291da0a15cc1137c837f9e6d2454e4
Anthony Ho <AnthonyHo0728@gmail.com>
2021-08-31 13:52:11 -0700

    Working with default reply

---
+++ Phase 3/Chat/chatClient.js
@@ -0,0 +1,19 @@
+let ws = new WebSocket("ws://localhost:9090");
+
+// ws.onopen = () => {
+//     ws.send("Hi SERVER HIHIHIHIH");
+// }
+
+function getMessage(uName,uMessage) {
+    let newMessage = uName.value + " says: " + uMessage.value;
+    ws.send(newMessage);
+    let chatLog = document.getElementById("chat");
+    let newLine = document.createElement("p");
+    newLine.innerHTML = newMessage;
+    chatLog.appendChild(newLine);
+    ws.onmessage = (msg) => {
+        let replyLine = document.createElement("p");
+        replyLine.innerHTML= "Server says: " + msg.data;
+        chatLog.appendChild(replyLine);
+    }
+}
--- Phase 3/Chat/server.js
+++ Phase 3/Chat/server.js
@@ -1,3 +1,23 @@
-let net = require("net");
+let express = require("express");
+let app = express();
 
-let server = net.createServer();
+let ws = require("express-ws")(app);
+
+app.ws("/",(socket,request)=> {
+    console.log("Client Connected!");
+
+    // recieve message from client application 
+    socket.on("message",(msg)=> {
+        let currDate = new Date();
+        let dateEntry = (currDate.getMonth()+1) + "/" + currDate.getDate() + "/" + currDate.getFullYear();
+        let timeEntry = currDate.getHours() +":"+currDate.getMinutes()+":"+currDate.getSeconds();
+        let reply = "Message received on: " + dateEntry + " at " + timeEntry;
+        console.log(msg);
+        console.log("Server Replied: " + reply);
+        socket.send(reply);
+    });
+
+    
+})
+
+app.listen(9090,()=>console.log("Server running on port number 9090"));
