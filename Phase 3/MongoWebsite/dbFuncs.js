434a96bafb1d4b9b51a51bbb7d40e61796941965
Anthony Ho <AnthonyHo0728@gmail.com>
2021-09-07 13:04:45 -0700

    Finished add

--- Phase 3/MongoWebsite/dbFuncs.js
+++ Phase 3/MongoWebsite/dbFuncs.js
@@ -1,21 +1,62 @@
 let mongoose = require("mongoose");
 let express = require("express");
 let bodyParser = require("body-parser");
-let app = express();
 
+let app = express();
+let url = "mongodb://localhost:27017/tcsmean";
+mongoose.pluralize(null);   
+mongoose.connect(url).then(res=>console.log("connected")).catch(err=>console.log(err));
+let courseSchema = mongoose.Schema({
+    _id:String,
+    cname:String,
+    cdesc:String,
+    amt:Number
+});
+let courseModel = mongoose.model("Courses", courseSchema);
+mongoose.disconnect();
+
+function callDB(fields,func) {
+    console.log(fields.id);
+        if(func=="add") {
+            mongoose.connect(url).then(res=>console.log("connected")).catch(err=>console.log(err));
+            let db = mongoose.connection;
+            db.once("open",() => {
+            let newCourse = new courseModel({_id:fields.id,pname:fields.cName, cdesc:fields.desc, amt:fields.amt});
+            courseModel.insertMany(newCourse,(err,result)=> {
+                if(!err){
+                    console.log(result)
+                } else {
+                    console.log(err);
+                }
+                mongoose.disconnect();  
+            })
+        })
+        } else if( func=="delete") {
+            console.log("deleting");
+        } else if( func=="update") {
+            console.log("updating");
+        }
+}
+
+app.get("/",(request,response)=> {
+    response.sendFile(__dirname+"\\index.html");
+})
 app.get("/addCourse",(request,response)=> {
-    //response.send("Welcome to add page");
     response.sendFile(__dirname+"\\addCourse.html");
+    if(request.query.id != undefined) {
+        callDB(request.query,"add");
+    }
 })
 app.get("/updateCourse",(request,response)=> {
-    //response.send("Welcome to update page");
     response.sendFile(__dirname+"\\updateCourse.html");
+    callDB(request.query,"update");
 })
 app.get("/deleteCourse",(request,response)=> {
-    //response.send("Welcome to delete page");
     response.sendFile(__dirname+"\\deleteCourse.html");
+    callDB(request.query,"delete");
 })
 app.get("/fetchCourse",(request,response)=> {
-    //response.send("Welcome to fetch page");
     response.sendFile(__dirname+"\\fetchCourse.html");
 })
+
+app.listen(9090,()=>console.log("Server running on port number 9090"))
